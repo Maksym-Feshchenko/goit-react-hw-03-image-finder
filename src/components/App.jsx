e68f960e7a1fc5539a9e38d2c6b01ef74fe78103
Maksym-Feshchenko <maksym.fd@gmai.com>
2023-05-16 00:26:24 +0300

    new repozitory, fix git hub

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,116 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import  React, { Component }  from "react";
+
+import Searchbar from "./Searchbar/Searchbar";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import Button from "./Button/Button";
+import loadMore from './PixabayAPI';
+import Loader from "./Loader/Loader";
+
+import "../styles.css";
+
+class App extends Component {
+  state = {
+    images: [],
+    
+    page: 1,
+    searchQuery: "",    
+
+    showModal: false,
+    modalImage: '',
+
+    showButton: false,
+
+    isLoading: false
+  };
+
+  openModal = (largeImageURL) => {
+    this.setState({ showModal: true, webformatURL: largeImageURL});
+  }
+
+  toggleModal = () => {
+    this.setState({
+      modalImage: '',
+      showModal: false,
+    });
+  }
+
+  handleFormSubmit = (searchQuery) => {
+    this.setState({ searchQuery, page: 1, images: [], showButton: true });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.fetchImages();
+    }
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  }
+
+  fetchImages = () => {
+    const { page, searchQuery } = this.state;
+    const API_KEY = "35073531-a3301b6130ef0984d8d454ab2";
+    const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&per_page=12&page=${page}`;
+    this.setState({ isLoading: true });
+
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...data.hits],
+        }));
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      })
+  };
+
+  handleLoadMore = () => {
+    if (this.setState((prevState) => ({ page: prevState.page + 1 }))) {
+    
+    };
+  };
+
+  loadMoreImages = () => {
+    const { searchQuery, page } = this.state;
+    const nextPage = page + 1;
+
+    this.setState({ isLoading: true });
+  
+    loadMore(searchQuery, nextPage).then((newImages) => {
+      if (newImages.length > 0) {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...newImages],
+          page: prevState.page + 1,
+        }));
+      }
+    })
+    .finally(() => {
+      this.setState({ isLoading: false });
+    });
+  };
+  
+
+  render() {
+    const { images, showButton, isLoading} = this.state;
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleFormSubmit} />
+
+        
+
+        <ImageGallery images={images} openModal={this.openModal} />
+
+        {/* <Loader /> */}
+        
+        {showButton && (<Button onButtonClick={this.loadMoreImages} isHidden={images.length === 0} /> )}
+        {isLoading && <Loader />}
+
+      </div>
+    );
+  }
+}
+
+export default App
